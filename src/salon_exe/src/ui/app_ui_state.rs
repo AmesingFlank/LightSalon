192a792cc6499208b1d8090e5934b021971d3a5e
Dunfan Lu <dunfanlu@meta.com>
2023-10-27 10:17:27 -0400

    small refactor

--- src/salon_exe/src/ui/app_ui_state.rs
+++ src/salon_exe/src/ui/app_ui_state.rs
@@ -1,12 +1,8 @@
-use std::time::{SystemTime};
+use std::time::SystemTime;
 
 pub struct AppUiState {
     pub last_frame_size: Option<(f32, f32)>,
-
-    pub last_fps: f32,
-    pub last_fps_record_time: SystemTime,
-    pub frames_since_last_fps_record: u32,
-
+    pub fps_counter: FpsCounterState,
     pub selected_curve_control_point_index: Option<usize>,
 }
 
@@ -14,10 +10,24 @@ impl AppUiState {
     pub fn new() -> Self {
         AppUiState {
             last_frame_size: None,
+            fps_counter: FpsCounterState::new(),
+            selected_curve_control_point_index: None,
+        }
+    }
+}
+
+pub struct FpsCounterState {
+    pub last_fps: f32,
+    pub last_fps_record_time: SystemTime,
+    pub frames_since_last_fps_record: u32,
+}
+
+impl FpsCounterState {
+    pub fn new() -> Self {
+        FpsCounterState {
             last_fps: 0.0,
             last_fps_record_time: SystemTime::now(),
             frames_since_last_fps_record: 0u32,
-            selected_curve_control_point_index: None,
         }
     }
 }
--- src/salon_exe/src/ui/bottom_bar.rs
+++ src/salon_exe/src/ui/bottom_bar.rs
@@ -8,17 +8,18 @@ use std::time::{SystemTime, UNIX_EPOCH};
 
 
 pub fn bottom_bar(ui: &mut Ui, session: &mut Session, ui_state: &mut AppUiState) {
+    let fps_counter = &mut ui_state.fps_counter;
     let now = SystemTime::now();
     let duration_since_last_record = now
-        .duration_since(ui_state.last_fps_record_time)
+        .duration_since(fps_counter.last_fps_record_time)
         .expect("Time went backwards").as_secs_f32();
-    let mut fps = ui_state.last_fps;
-    ui_state.frames_since_last_fps_record += 1u32;
+    let mut fps = fps_counter.last_fps;
+    fps_counter.frames_since_last_fps_record += 1u32;
     if duration_since_last_record > 1.0 {
-        fps = ui_state.frames_since_last_fps_record as f32 / duration_since_last_record;
-        ui_state.last_fps_record_time = now;
-        ui_state.last_fps = fps;
-        ui_state.frames_since_last_fps_record = 0u32;
+        fps = fps_counter.frames_since_last_fps_record as f32 / duration_since_last_record;
+        fps_counter.last_fps_record_time = now;
+        fps_counter.last_fps = fps;
+        fps_counter.frames_since_last_fps_record = 0u32;
     }
     let fps_msg = "FPS: ".to_owned() + fps.to_string().as_str();
     ui.label(fps_msg);
