fa2f6a34644eb64dc44de0ae5ba6ff5d5241c2e9
Dunfan Lu <dunfanlu@meta.com>
2023-11-04 23:50:51 -0400

    make a copy of the egui::Slider class, so that we can extend it to support colors

--- src/salon_exe/src/ui/widgets/mod.rs
+++ src/salon_exe/src/ui/widgets/mod.rs
@@ -1,7 +1,9 @@
 mod colored_radio_button;
+mod colored_slider;
 mod main_image;
 mod thumbnail;
 
 pub use colored_radio_button::*;
+pub use colored_slider::*;
 pub use main_image::*;
 pub use thumbnail::*;
