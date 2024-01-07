2ebef971d373232cb8fdf850d8b771259202df21
Dunfan Lu <dunfanlu@meta.com>
2024-01-07 00:44:25 -0500

    use TableBuilder for masking ui

--- src/salon_exe/src/ui/masking.rs
+++ src/salon_exe/src/ui/masking.rs
@@ -4,31 +4,40 @@ use eframe::{
     egui::{self, CollapsingHeader, Ui},
     egui_wgpu,
 };
+use egui_extras::{Column, TableBuilder};
 use salon_core::{
     editor::{Edit, GlobalEdit, MaskedEdit},
     ir::{Mask, MaskPrimitive, MaskTerm, RadialGradientMask},
     session::Session,
 };
 
-use super::{utils::get_image_size_in_ui, widgets::MaskIndicatorCallback, AppUiState};
+use super::{widgets::MaskIndicatorCallback, AppUiState};
 
 pub fn masking(ui: &mut Ui, session: &mut Session, ui_state: &mut AppUiState, edit: &mut Edit) {
     CollapsingHeader::new("Masking")
         .default_open(true)
         .show(ui, |ui| {
             ui.group(|ui| {
-                egui::Grid::new("my_grid")
-                    .num_columns(1)
-                    .striped(true)
-                    .show(ui, |ui| {
-                        for i in 0..edit.masked_edits.len() {
+                let mut table = TableBuilder::new(ui).column(Column::auto()).cell_layout(
+                    egui::Layout::left_to_right(egui::Align::LEFT)
+                        .with_cross_align(egui::Align::Center),
+                );
+                let row_height = ui_state.last_frame_size.unwrap().1 * 0.04;
+                let image_height = row_height * 0.8;
+                table.body(|mut body| {
+                    body.rows(row_height, edit.masked_edits.len(), |i, mut row| {
+                        row.col(|ui| {
                             if ui.radio(ui_state.selected_mask_index == i, "").clicked() {
                                 ui_state.selected_mask_index = i;
                             }
                             if let Some(ref result) = session.editor.current_result {
                                 let mask_img = result.masked_edit_results[i].mask.clone();
-
-                                let size = get_image_size_in_ui(ui, &mask_img);
+                                let aspect_ratio = mask_img.aspect_ratio();
+                                let image_width = image_height / aspect_ratio;
+                                let size = egui::Vec2 {
+                                    x: image_width,
+                                    y: image_height,
+                                };
                                 let (rect, response) =
                                     ui.allocate_exact_size(size, egui::Sense::click_and_drag());
                                 ui.painter().add(egui_wgpu::Callback::new_paint_callback(
@@ -39,9 +48,9 @@ pub fn masking(ui: &mut Ui, session: &mut Session, ui_state: &mut AppUiState, ed
                                 ));
                             }
                             ui.label(&edit.masked_edits[i].name);
-                            ui.end_row()
-                        }
+                        });
                     });
+                });
             });
             ui.menu_button("Create New Mask", |ui| {
                 if ui.button("Radial Gradient").clicked() {
